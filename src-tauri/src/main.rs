5475fa48e551e6f3ac6bb408eb7a6cbea0136b66
Donut-the-1st <93704931+Donut-the-1st@users.noreply.github.com>
2023-08-31 21:15:49 +1200

    Fixed a *whole* load of returns

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,10 +5,40 @@ mod types;
 mod power;
 mod argsort;
 
+
+
+use core::result::*;
 use argsort::*;
 use types::*;
 use power::{rs_power};
 use std::sync::*;
+
+// begin code from Simon Hyll
+// https://hackmd.io/@simonhyll/r1O1reUr2#Shortly-about-errors
+#[derive(Debug, thiserror::Error)]
+enum Error {
+  #[error(transparent)]
+  Io(#[from] std::io::Error),
+  #[error("the mutex was poisoned")]
+  PoisonError(String),
+}
+
+impl serde::Serialize for Error {
+  fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::ser::Serializer,
+  {
+    serializer.serialize_str(self.to_string().as_ref())
+  }
+}
+
+impl<T> From<std::sync::PoisonError<T>> for Error {
+  fn from(err: std::sync::PoisonError<T>) -> Self {
+    Error::PoisonError(err.to_string())
+  }
+}
+// end code from Simon Hyll
+
 
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -16,7 +46,7 @@ fn greet(name: &str) -> String {
 }
 
 #[tauri::command]
-async fn add_player(state: tauri::State<'_, Mutex<Competition>>, player_name: String) -> Competition {
+async fn add_player(state: tauri::State<'_, Mutex<Competition>>, player_name: String) -> Result<(), Error> {
   let mut locked_state = state.lock()?;
   locked_state.initialised = false;
   let new_player: Player = Player {
@@ -24,22 +54,22 @@ async fn add_player(state: tauri::State<'_, Mutex<Competition>>, player_name: St
     ID: u8::from(locked_state.players.len())
   };
   locked_state.players.push(new_player);
-  locked_state.clone()
+  Ok(())
 }
 
 #[tauri::command]
-async fn initialise(state: tauri::State<'_, Mutex<Competition>>) -> Competition {
+async fn initialise(state: tauri::State<'_, Mutex<Competition>>) -> Result<(), Error> {
   let mut locked_state = state.lock()?;
   let num_players:u8 = u8::from(locked_state.players.len());
   locked_state.games_won = Array2::<u8>::zeros((num_players, num_players));
   locked_state.prop = Array2::<f64>::zeros((num_players, num_players));
   locked_state.rankings = Array1::<f64>::zeros(num_players);
   locked_state.initialised = true;
-  locked_state.clone()
+  Ok(())
 }
 
 #[tauri::command]
-async fn add_result(state: tauri::State<'_, Mutex<Competition>>, result: String) -> Competition {
+async fn add_result(state: tauri::State<'_, Mutex<Competition>>, result: String) -> Result<(), Error> {
   //Check if we can conform result to a result before we lock the thread
   let result: Result = serde_json::from_str(&result)?;
   let mut locked_state = state.lock()?;
@@ -51,18 +81,21 @@ async fn add_result(state: tauri::State<'_, Mutex<Competition>>, result: String)
   locked_state.games_played[p2_p1] += result.player_1_wins + result.player_2_wins;
   locked_state.prop[p1_p2] = f64::from(locked_state.games_won[p1_p2])/f64::from(locked_state.games_played[p1_p2]);
   locked_state.prop[p2_p1] = f64::from(locked_state.games_won[p2_p1])/f64::from(locked_state.games_played[p2_p1]);
-  locked_state.clone()
+  Ok(())
 }
 
 #[tauri::command]
-async fn update_results(state: tauri::State<'_, Mutex<Competition>>) -> Competition {
+async fn update_results(state: tauri::State<'_, Mutex<Competition>>) -> Result<(), Error> {
   let mut locked_state = state.lock()?;
   let (new_rankings, _) = rs_power(locked_state.prop.view(), 1e-2);
   locked_state.rankings.assign(&new_rankings);
-  locked_state.clone()
+  Ok(())
 }
 
+#[tauri::command]
+async fn get_placements(state: tauri::State<'_, Mutex<Competition>>) -> Result<String, Error> {
 
+}
 
 fn main() {
   tauri::Builder::default()
