e0cc92e43eb8479e0bc1a81cdf6353a2fd3413a6
Donut-the-1st <93704931+Donut-the-1st@users.noreply.github.com>
2023-08-31 12:33:31 +1200

    Improved Competition Default, added Initialalised

--- src-tauri/src/types.rs
+++ src-tauri/src/types.rs
@@ -5,11 +5,26 @@ pub use serde::{Deserialize, Serialize};
 pub struct Player {
     name: String
 }
+
 #[derive(Serialize, Deserialize, Default)]
 pub struct Competition {
     pub(crate) players: Vec<Player>,
     games_played: Array2<u8>,
     games_won: Array2<u8>,
     prop: Array2<f64>,
-    rankings: Array1<f64>
+    rankings: Array1<f64>,
+    initialised: bool
+}
+
+impl Default for Competition {
+    fn default() -> Self {
+        Competition{
+            players: vec![],
+            games_played: Default::default(),
+            games_won: Default::default(),
+            prop: Default::default(),
+            rankings: Default::default(),
+            initialised: false
+        }
+    }
 }
